b244f3fd66e4e355933f7b4b94bf5e39a742b900
alecastroitcr <alecastro@ic-itcr.ac.cr>
2023-03-12 04:23:25 +0000

    Pagina de 404 si se intenta ingresar a URL no existente agregada

--- backend/website/views.py
+++ backend/website/views.py
@@ -2,10 +2,6 @@ from flask import Blueprint,render_template, request,flash
 
 views = Blueprint('views', __name__)
 
-@views.route('/404',methods=['GET','POST'])
-def error404():
-    return render_template('404.html')
-
 @views.route('/',methods=['GET','POST'])
 def home():
     return "TuRingo home"
