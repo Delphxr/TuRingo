37eeff10bae43ae76fb677f08da9dc0dde1f1f1a
Alejandro Castro Araya <90380164+alecastroitcr@users.noreply.github.com>
2023-04-05 01:21:07 -0600

    Update setup.py

--- backend/setup.py
+++ backend/setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='turingo',
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'Flask',
-        'Flask-PyMongo'
+        'PyMongo'
     ],
 )
