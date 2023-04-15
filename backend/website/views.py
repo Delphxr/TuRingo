c40f1b285d7402df3cd1a7ab1a751bb0654e4889
alecastroitcr <alecastro@ic-itcr.ac.cr>
2023-04-15 09:39:31 +0000

    Funciones de operaciones de BD agregadas

--- backend/website/views.py
+++ backend/website/views.py
@@ -2,7 +2,39 @@ from flask import Blueprint,render_template, request,flash
 from website import turing
 import json
 
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 views = Blueprint('views', __name__)
+
+def borrar_usuario(usuario_id):
+    result = usuarios.delete_one({'_id': ObjectId(usuario_id)})
+    if result.deleted_count == 1:
+        return jsonify({'message': 'Usuario borrado con éxito'})
+    else:
+        return jsonify({'message': 'Usuario no fue encontrado'})
+
+def insertar_usuario():
+    username = request.form['username']
+    password = request.form['password']
+    correo = request.form['correo']
+    tipo_usuario = request.form['tipo_usuario']
+
+    usuario = {
+        'username': username,
+        'password': password,
+        'correo': correo,
+        'tipo_usuario': tipo_usuario
+    }
+    result = usuarios.insert_one(usuario)
+    return f'Usuario agregado con el id {result.inserted_id}'
+
+def get_usuarios():
+    from app import usuarios
+    lista_usuarios = usuarios.find()
+
+    return lista_usuarios
 
 @views.route('/',methods=['GET','POST'])
 def home():
@@ -14,6 +46,8 @@ def editor():
 
 @views.route('/estudiante', methods=['GET','POST'])
 def estudiantes():
+    lista = get_usuarios()
+    print(lista)
     return render_template("estudiante.html")
 
 @views.route('/administrador', methods=['GET','POST'])
